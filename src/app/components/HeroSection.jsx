5c8500ab09dca0d85281e7d8a64eccc40557de9f
james <jleo56291@gmail.com>
2024-04-16 13:05:05 +0300

    updates

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -62,9 +62,9 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/james.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
